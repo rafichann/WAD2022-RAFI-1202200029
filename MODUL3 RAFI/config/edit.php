60825cb4657b40b48c9d79c0294e3626c20bede9
User <chanxstep@gmail.com>
2022-11-25 09:04:42 +0700

    final

--- MODUL3 RAFI/config/edit.php
+++ MODUL3 RAFI/config/edit.php
@@ -12,3 +12,22 @@ $gambar = $_FILES['gambar']['name'];
 
 $target = "../asset/images/";
 
+if ("SELECT foto_mobil FROM showroom_rafi_table WHERE id_mobil = $id" != 0) {
+    if (move_uploaded_file($_FILES['gambar']['tmp_name'], $target . $gambar)) {
+      $editquery = "UPDATE showroom_rafi_table SET nama_mobil = '$namamobil', pemilik_mobil = '$pemilik', merk_mobil = '$merk', tanggal_beli = '$tanggalbeli', deskripsi = '$desc', foto_mobil = '$gambar', status_pembayaran = '$status' WHERE id_mobil = $id";
+      if (mysqli_query($connector, $editquery)) {
+        header("location: ../pages/ListCar-Rafi.php?pesan=update");
+      } else {
+        header("location: ../pages/ListCar-Rafi.php?pesan=gagal");
+      }
+    } else {
+      header("location: ../pages/ListCar-Rafi.php?pesan=gagal");
+    }
+} else {
+    $editquery = "UPDATE showroom_rafi_table SET nama_mobil = '$namamobil', pemilik_mobil = '$pemilik', merk_mobil = '$merk', tanggal_beli = '$tanggalbeli', deskripsi = '$desc', status_pembayaran = '$status' WHERE id_mobil = $id";
+    if (mysqli_query($connector, $editquery)) {
+      header("location: ../pages/ListCar-Rafi.php?pesan=update");
+    } else {
+      header("location: ../pages/ListCar-Rafi.php?pesan=gagal");
+    }
+}
